e9c9a9a09995b419977bf18416f2ffe82604146f
Tony Xiong <14858520@qq.com>
2021-10-24 04:11:12 -0700

    removed BeanDBUserIntf.h

--- src/org/jinsha/bean/BeanWorld.h
+++ src/org/jinsha/bean/BeanWorld.h
@@ -7,7 +7,6 @@
 #include "./common.h"
 #include "./Property.h"
 #include "./Bean.h"
-#include "./BeanDBUserIntf.h"
 #include "./AbstractBeanDB.h"
 #include "./Page.hpp"
 
@@ -19,7 +18,7 @@ class Bean;
 class Property;
 class AbstractBeanDB;
 
-class BeanWorld : public BeanDBUserIntf
+class BeanWorld
 {
 public:
 
@@ -35,49 +34,114 @@ public:
      ***********************************************************/
 public:
     /**
-     * @ref BeanDBUserIntf::defineProperty()
+     * Define a property.
+     * 
+     * Property must be defined before it can be used.
+     * 
+     * Notes:
+     * - Property is type sensitive: i.e. setting the property with a value of type other
+     *    than the one defined here will fail.
+     * 
+     * @param name the name of property
+     * @param valueType the value type of property
+     * @return the pointer to the property instance
      */
-    virtual Property* defineProperty(const char* name, Property::ValueType valueType) override;
+    virtual Property* defineProperty(const char* name, Property::ValueType valueType);
 
     /**
-     * @ref BeanDBUserIntf::defineArrayProperty()
+     * Define an array property.
+     * 
+     * Array property must be defined before it can be used.
+     * 
+     * Notes:
+     * - Array property is type sensitive: i.e. adding to the array property 
+     *   with a value of type other than the one defined here will fail.
+     * 
+     * @param name the name of property
+     * @param valueType the value type of the element of the array property
+     * @return the pointer to the property instance
      */
-    virtual Property* defineArrayProperty(const char* name, Property::ValueType valueType) override;
+    virtual Property* defineArrayProperty(const char* name, Property::ValueType valueType);
 
     /**
-     * @ref BeanDBUserIntf::defineRelation()
+     * Define a relation property.
+     * 
+     * Relation is a special kind of property, which represents relationship between
+     * two beans, e.g. father/mother etc.
+     * 
+     * @param name the name of relation property
+     * @return the pointer to the property instance
      */
-    virtual Property* defineRelation(const char* name) override;
+    virtual Property* defineRelation(const char* name);
 
     /**
-     * @ref BeanDBUserIntf::defineArrayRelation()
+     * Define an array relation property.
+     * 
+     * Array relation property must be defined before it can be used.
+     * 
+     * @param name the name of array relation property
+     * @return the pointer to the property instance
      */
-    virtual Property* defineArrayRelation(const char* name) override;
+    virtual Property* defineArrayRelation(const char* name);
 
     /**
-     * @ref BeanDBUserIntf::undefineProperty()
+     * Undefine a property.
+     * 
+     * Notes:
+     * - This method can be used to undefine either a property, an array property,
+     *    a relation, or an array relation;
+     * - CAUTION: All beans that have this property will remove the property with this id.
+     * 
+     * @param property the property to be undefined
+     * @return 0 on success, or an error code
      */
-     virtual int undefineProperty(Property* property) override;
+    virtual int undefineProperty(Property* property);
 
     /**
-     * @ref BeanDBUserIntf::getProperty(const char* name)
+     * Undefine a relation.
+     * 
+     * Notes:
+     * - Same as undefineProperty() (just an alias).
+     * 
+     * @param relation the relation to be undefined
+     * @return 0 on success, or an error code
      */
-    virtual Property* getProperty(const char* name) override;
+    virtual int undefineRelation(Property* relation) {return undefineProperty(relation);};
 
     /**
-     * @ref BeanDBUserIntf::getProperty(pidType pid)
+     * Get property/relation/array property/array relation by name.
+     * 
+     * @param name property name
+     * @return property
      */
-    virtual Property* getProperty(pidType id) override;
+    // virtual const Property* getProperty(const char* name) const;
+    virtual Property* getProperty(const char* name);
 
     /**
-     * @ref BeanDBUserIntf::getProperties()
+     * Get property/relation/array property/array relation by id.
+     * 
+     * @param id property id
+     * @return property
      */
-    virtual const std::unordered_map<std::string, Property*>& getProperties() override;
+    virtual Property* getProperty(pidType id);
 
-    // /**
-    //  * @ref BeanDBUserIntf::getRelations()
-    //  */
-    // virtual const std::unordered_map<std::string, Property*>& getRelations() override;
+    /**
+     * Get relation.
+     * 
+     * Notes:
+     * - Same as getProperty() (just an alias).
+     * 
+     * @param name the name of relation
+     * @return 0 on success, or an error code
+     */
+    virtual Property* getRelation(const char* name) {return getProperty(name);};
+
+    /**
+     * Get all properties (including all properties/relations).
+     * 
+     * @return a map containing all properties.
+     */
+    virtual const std::unordered_map<std::string, Property*>& getProperties();
 
 
     /***********************************************************
@@ -85,32 +149,25 @@ public:
      ***********************************************************/
 public:
     /**
-     * @ref BeanDBUserIntf::createBean()
+     * Creat an empty bean.
+     * 
+     * @return the pointer pointing to the bean, or nullptr if exception occurs.
      */
-    virtual Bean* createBean() override;
+    virtual Bean* createBean();
 
     /**
-     * @ref BeanDBUserIntf::getBean()
+     * Get bean by id.
+     * 
+     * @param id the id of the bean
+     * @param loadFromDB if true, try to load the bean from db
+     *                    when the bean has not been loaded into this world.
+     * @return the pointer pointing to the bean, or null if no such
+     *                   bean exist.
      */
-    virtual Bean* getBean(oidType id, bool loadFromDB = true) override;
+    virtual Bean* getBean(oidType id, bool loadFromDB = true);
 
     /**
-     * @ref BeanDBUserIntf::deleteBean()
-     */
-    virtual int deleteBean(Bean* bean) override;
-
-    /**
-     * @ref BeanDBUserIntf::reloadAll()
-     */
-    virtual int reloadAll() override;
-
-    /**
-     * @ref BeanDBUserIntf::saveAll()
-     */
-    virtual int saveAll() override;
-
-    /**
-     * Unload (remove) a bean from this world. 
+     * Unload a bean from this world. 
      * 
      * Notes:
      * - the memory allocated to bean will be deleted 
@@ -118,9 +175,37 @@ public:
      * - however the bean will NOT be deleted from the 
      *    database;
      * 
-     * @param bean the bean to be unloaded
+     * @param bean the bean to be uloaded
      */
-    virtual void unloadBean(Bean* bean) override;
+    virtual void unloadBean(Bean* bean);
+
+    /**
+     * Remove a single bean from the storage.
+     * 
+     * This method is supposed to be called from class BeanWorld.
+     * 
+     * @param bean the bean to be removed
+     * @return 0 for success, or an error code
+     */
+    virtual int deleteBean(Bean* bean);
+
+    /**
+     * Reload all data from the storage.
+     * 
+     * Notes:
+     * - all previously loaded data will be removed from this world
+     *    before reloading;
+     * 
+     * @return 0 for success, or an error code
+     */
+    virtual int reloadAll();
+
+    /**
+     * Save all data into the storage.
+     * 
+     * @return 0 for success, or an error code
+     */
+    virtual int saveAll();
 
     /**
      * Get total number of beans chached in this world (in memory).
@@ -179,6 +264,7 @@ public:
      * @ref BeanDBPIntf
      */
     virtual BeanIdPage* findObjects(const Property* property, unsigned int pageSize = BeanDBIntf::DEFAULT_PAGE_SIZE) const;
+
 
 private:
     /**
